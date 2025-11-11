d79a29d954a0ed808c0ff8bdb95eb2b0d5c8cbb8
David Cruz <da.cruz84@uniandes.edu.co>
2025-11-10 19:16:51 -0500

    HU03 Se agrega el uso de corutinas para no bloquear el hilo principal

--- app/src/main/java/com/miso/vinilo/data/adapter/NetworkServiceAdapterMusician.kt
+++ app/src/main/java/com/miso/vinilo/data/adapter/NetworkServiceAdapterMusician.kt
@@ -6,6 +6,9 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 /**
  * Sealed class that represents the result of a network operation.
@@ -17,18 +20,25 @@ sealed class NetworkResult<out T> {
 
 /**
  * Network service adapter that uses a network API to retrieve musician data.
- * Other classes should call this implementation directly.
- * @property api The [com.miso.vinilo.data.adapter.retrofit.MusicianApi] instance used to perform network requests.
+ * The dispatcher used for IO is injected so tests can pass a TestDispatcher.
+ * @property api The [MusicianApi] instance used to perform network requests.
+ * @property ioDispatcher Dispatcher used for IO operations (defaults to [Dispatchers.IO]).
  */
-class NetworkServiceAdapterMusicians(private val api: MusicianApi) {
+class NetworkServiceAdapterMusicians(
+    private val api: MusicianApi,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) {
 
     /**
      * Fetches a list of musicians from the network API.
+     * Executes the call on the injected IO dispatcher to avoid blocking the UI thread.
      * @return A [NetworkResult] containing a list of [MusicianDto] on success or an [Error].
      */
     suspend fun getMusicians(): NetworkResult<List<MusicianDto>> {
         return try {
-            val dtos: List<MusicianDto> = api.getMusicians()
+            val dtos: List<MusicianDto> = withContext(ioDispatcher) {
+                api.getMusicians()
+            }
             NetworkResult.Success(dtos)
         } catch (e: Exception) {
             NetworkResult.Error("Unknown network error", e)
